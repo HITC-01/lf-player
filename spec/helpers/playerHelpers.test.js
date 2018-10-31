7ff2b3ede3770e249c2360a9e6097398e5a491d2
lfelberg <lfelberg@berkeley.edu>
2018-10-30 19:03:51 -0700

    Tests for colorBar added

--- spec/helpers/playerHelpers.test.js
+++ spec/helpers/playerHelpers.test.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 import helpers from '../../client/helpers/playerHelpers.js';
 
 describe('createSongBar', () => {
@@ -30,19 +31,57 @@ describe('convertToMinSec', () => {
   });
 });
 
+
+// Tests for COLORBAR
+const barFrac = 0.5;
+let playStats = { };
+
 describe('colorBar', () => {
-  // test('', () => {
-  //   expect().toBe();
-  // });
-  // test('', () => {
-  //   expect().toBe();
-  // });
-  // test('', () => {
-  //   expect().toBe();
-  // });
-  // test('', () => {
-  //   expect().toBe();
-  // });
+  beforeEach(() => {
+    playStats = {
+      currentTime: 0,
+      totalTime: 100,
+      playing: false,
+      hovering: false,
+      hoverPosition: 0.3,
+    };
+  });
+
+  test('makes a string indicating that song is not playing, position is to be played', () => {
+    expect(helpers.colorBar(barFrac, playStats)).toBe('-to-play');
+  });
+
+  test('makes a string indicating that song is not playing, hovering, position is to be played', () => {
+    playStats.hovering = true;
+    expect(helpers.colorBar(barFrac, playStats)).toBe('-to-play');
+  });
+
+  test('makes a string indicating that the song is being played and position is to be played', () => {
+    playStats.playing = true;
+    playStats.currentTime = 30;
+    expect(helpers.colorBar(barFrac, playStats)).toBe('-playing-to-play');
+  });
+
+  test('makes a string indicating that song is playing, position is played', () => {
+    playStats.playing = true;
+    playStats.currentTime = 70;
+    expect(helpers.colorBar(barFrac, playStats)).toBe('-playing');
+  });
+
+  test('makes a string indicating that song is playing, position is between hover and playing', () => {
+    playStats.playing = true;
+    playStats.hovering = true;
+    playStats.currentTime = 70;
+    expect(helpers.colorBar(barFrac, playStats)).toBe('-hover-middle');
+  });
+
+  test('makes a string indicating that song is playing, position is between hover and playing', () => {
+    playStats.playing = true;
+    playStats.hovering = true;
+    playStats.currentTime = 30;
+    playStats.hoverPosition = 0.9;
+    expect(helpers.colorBar(barFrac, playStats)).toBe('-hover-middle');
+  });
 });
 
 // describe('', () => {
