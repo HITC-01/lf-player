33c9cb8dd8276d5e636ab64f111aadcd4a86e616
lfelberg <lfelberg@berkeley.edu>
2018-10-31 10:42:30 -0700

    Beginning tests with Enzyme
    TODO:
    react tests for each component
      - player
      - song art
      - song bar
      - song bar entry
      - song comments
      - song display
      - song extras
      - song main
      - song tracker

--- spec/components/songBarEntry.test.js
+++ spec/components/songBarEntry.test.js
@@ -1,12 +1,51 @@
 /* eslint-env jest */
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from '../enzyme';
 import SongBarEntry from '../../client/components/SongBarEntry.jsx';
 
 describe('SongBarEntry component', () => {
-  test('Should render correctly', () => {
+  let props = {
+    position: 'upper',
+    number: 200,
+    bar: 0.5,
+    playState: {},
+    barFraction: ,
+    handleScan,
+    handleExit,
+    handleClick,
+  };
+
+  test('Should render single song bar bar', () => {
     const component = renderer.create(<SongBarEntry />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('List tests', () => {
+
+  it('renders list-items', () => {
+    const items = ['one', 'two', 'three'];
+    const wrapper = shallow(<List items={items} />);
+
+    // Expect the wrapper object to be defined
+    expect(wrapper.find('.list-items')).toBeDefined();
+    expect(wrapper.find('.item')).toHaveLength(items.length);
+  });
+
+  it('renders a list item', () => {
+    const items = ['Thor', 'Loki'];
+    const wrapper = shallow(<List items={items} />);
+
+    // Check if an element in the Component exists
+    expect(wrapper.contains(<li key='Thor' className="item">Thor</li >)).toBeTruthy();
+  });
+
+  it('renders correct text in item', () => {
+    const items = ['John', 'James', 'Luke'];
+    const wrapper = shallow(<List items={items} />);
+
+    //Expect the child of the first item to be an array
+    expect(wrapper.find('.item').get(0).props.children).toEqual('John');
   });
 });
