973b080e41c9d2cdfccdb27f1abc309ea4f584df
lfelberg <lfelberg@berkeley.edu>
2018-11-05 17:44:05 -0800

    Adding in more env vars for DB on AWS

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mysql = require('promise-mysql');
 
 module.exports = mysql.createPool({
   user: process.env.DB_USER,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  database: 'lfelberg',
 });
